efc796bfa08048af507fe6cddc7608b868cad775
OriolB-Uri <oriolbotella@hotmail.com>
2023-10-18 14:02:53 +0200

    ejercicio hecho

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@
 const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
 
 
+function changeColor(){
+    let randomSelection = colors[Math.floor(Math.random() * colors.length)];
+    document.querySelector("body").style.backgroundColor = randomSelection;
+}
+
